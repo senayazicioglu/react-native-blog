8f763f3a7d20537a684491537b965b7bbed7eb89
Sena Betül Yazıcıoğlu <79763384+senayazicioglu@users.noreply.github.com>
2023-11-07 17:29:31 +0300

    App.js

--- App.js
+++ App.js
@@ -4,20 +4,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IndexScreen from "./screens/IndexScreen";
 import CreateScreen from "./screens/CreateScreen";
-import BlogContext, { BlogProvider } from "./context/BlogContext";
+import BlogContext, { Provider } from "./context/BlogContext";
 import { useContext } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <BlogProvider>
+    <Provider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerTitle: "Blog Uygulaması" }}>
           <Stack.Screen name="Index" component={IndexScreen} />
           <Stack.Screen name="Create" component={CreateScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </BlogProvider>
+    </Provider>
   );
 }
