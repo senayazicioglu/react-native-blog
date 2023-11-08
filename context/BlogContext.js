69474214235ef78659d498bbb0b8dd67a44af7f3
Sena Betül Yazıcıoğlu <79763384+senayazicioglu@users.noreply.github.com>
2023-11-08 23:00:59 +0300

    BlogContext.js

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -4,7 +4,17 @@ import CreateDataContext from "./CreateDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: "Vue Js" }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 999999),
+          title: "Vue Js",
+        },
+      ];
+
+    case "delete_blogpost":
+      return state.filter((BlogPost) => BlogPost.id !== action.payload);
+
     default:
       return state;
   }
@@ -15,10 +25,17 @@ const addBlogPost = (dispatch) => {
     dispatch({ type: "add_blogpost" });
   };
 };
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
 export const { Context, Provider } = CreateDataContext(
   blogReducer,
   {
     addBlogPost,
+    deleteBlogPost,
   },
   []
 );
